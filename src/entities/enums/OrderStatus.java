9b25ea09bf86100b771079527f4818d2a38efd2a
paulokiki <vieiralinspaulo@gmail.com>
2019-06-29 16:09:20 -0300

    Aula de enumeradores

---
+++ src/entities/enums/OrderStatus.java
@@ -0,0 +1,8 @@
+package entities.enums;
+
+public enum OrderStatus {
+    PENDING_PAYMENTS, //AGUARDANDO PAGAMENTO
+    PROCESSING, // PROCESSANDO
+    SHIPPED, // ENVIADO
+    DELIVERED;    //ENTREGUE
+}

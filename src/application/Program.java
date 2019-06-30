ecae2caca21d7e91d63b7eeaf363b7c08ebe365d
paulokiki <vieiralinspaulo@gmail.com>
2019-06-30 12:37:51 -0300

    Teste de ArrayList instaciado

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,11 +3,13 @@ package application;
 
 import entities.Order;
 import entities.enums.OrderStatus;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 
 public class Program {
-    
+    List<Integer> lista = new ArrayList<Integer>();
     public static void main(String[] args){
         Order order =  new Order(1080, new Date(), OrderStatus.PENDING_PAYMENTS);
         
